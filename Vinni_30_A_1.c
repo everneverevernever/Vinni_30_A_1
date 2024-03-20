d13dd3845b7653513f6f9d1abfec900fcb69c47d
gleb_z <118896591+everneverevernever@users.noreply.github.com>
2024-03-21 02:55:35 +0300

    Add files via upload

--- Vinni_30_A_1.c
+++ Vinni_30_A_1.c
@@ -17,46 +17,78 @@
 Г2 Винни_Пух Пчеловодство
 */
 /** Решение. Гусятинер Л.Б., уровень A */
+/** Доделал решение. Зайцев Г.В, уровень ? */
 /** Этап 1. Считывание. Проверка.
-Начало решения 16.03.2024, 07:58
-Окончание решения 16.03.2024, 08:35
-Время 00:37 */
+Начало решения 16.03.2024, 22:58
+Окончание решения 16.03.2024, 2:53
+Время 2:53 */
 
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 #include <locale.h>
+#include <string.h>
 
 #define COLLEDGE_SIZE 1000
 
 typedef struct {
-	char gr[20];
-	char st[20];
-	char disc[20];
-	double mark;
+    char gr[20];
+    char st[20];
+    char disc[20];
+    double mark;
 } Record;
+
+typedef struct {
+    char gr[20];
+    char st[20];
+    char disc[20];
+    double mark;
+} Record2;
 
 int main(void) {
     char *locale = setlocale(LC_ALL, "");
-	Record v[COLLEDGE_SIZE];
+    Record v[COLLEDGE_SIZE];
+    Record2 v2[COLLEDGE_SIZE] = {0}; // Инициализация массива v2 нулями
+    FILE *in = fopen("in.txt", "r");
+    int n = 0;
+    Record rec;
+    char smark[20];
+    while (fscanf(in, "%s %s %s %s", rec.st, rec.gr, rec.disc, smark) == 4) {
+        rec.mark = atof(smark);
+        v[n] = rec;
+        n++;
+    }
+    fclose(in);
 
-	FILE *in = fopen("in.txt", "r");
-	int n = 0;
-	Record rec;
+    // Заполнение v2
+    for (int i = 0; i < n; i++) {
+        int j;
+        for (j = 0; j < n; j++) {
+            if (v[i].mark > v2[j].mark) {
+                strcpy(v2[j].gr, v[i].gr);
+                strcpy(v2[j].st, v[i].st);
+                strcpy(v2[j].disc, v[i].disc);
+                v2[j].mark = v[i].mark;
+                break;
+            }
+            if (v2[j].st[0] == '\0') { // Если студента еще нет в v2
+                strcpy(v2[j].gr, v[i].gr);
+                strcpy(v2[j].st, v[i].st);
+                strcpy(v2[j].disc, v[i].disc);
+                v2[j].mark = v[i].mark;
+                break;
+            }
+        }
+    }
 
-	char smark[20];
-	while (fscanf(in, "%s %s %s %s", rec.st, rec.gr, rec.disc, smark) == 4) {
-	    rec.mark = atof(smark);
+    // Вывод v2
+    FILE *out = fopen("out.txt", "w");
+    for (int i = 0; i < n; i++) {
 
-		v[n] = rec;
+        fprintf(out, "%s %s %s\n", v2[i].gr, v2[i].st, v2[i].disc);
+    }
+    fclose(out);
 
-		n++;
-	}
-	fclose(in);
-	for (int i = 0; i < n; i++) {
-        printf("%s %s %s %.2f\n", v[i].gr, v[i].st, v[i].disc, v[i].mark);
-	}
-
-	FILE *out = fopen("out.txt", "w");
-	fclose(out);
-	return 0;
+    return 0;
 }
+
+
